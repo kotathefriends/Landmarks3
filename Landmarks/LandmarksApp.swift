a463448a50dfb1406e78a539e099a4ed262d0683
kotathefriends <5070kouta@gmail.com>
2023-12-05 10:59:33 +0900

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by KOTA TAKAHASHI on 2023/12/05.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
